55167fff1f1846f2a167b5db3dfa1f4eb0324412
Nevo Cohen <nevocohen111@gmail.com>
2022-05-16 12:45:40 +0300

    Adventure game

---
+++ src/com/nevo/Location.java
@@ -0,0 +1,39 @@
+package com.nevo;
+
+import javax.swing.text.html.HTML;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+    private final int locationId;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+
+
+    public Location(int locationId, String description, Map<String, Integer> exits) {
+        this.locationId = locationId;
+        this.description = description;
+        if(exits != null)
+        this.exits = new HashMap<>(exits);
+        else this.exits = new HashMap<>();
+        this.exits.put("Q", 0);
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<>(exits);
+
+    }
+
+    public void addExit(String direction, int location) {
+        exits.put(direction, location);
+    }
+}
---
+++ src/com/nevo/Readme.txt
@@ -0,0 +1,1 @@
+Follow the instructions in order to use to program in the correct way,Enjoy!
